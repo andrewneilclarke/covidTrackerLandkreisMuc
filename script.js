8c88ed654b8c38b53caa9f51e94ae57f271de4be
Andrew Clarke <andrewneilclarke@gmail.com>
2021-05-03 16:01:05 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
-// const { request } = require("node:http");
 const county = document.querySelector('.county');
 const weeklyIncidence = document.querySelector('.weekly-incidence');
+let apiValues = [];
 
-// const request = new XMLHttpRequest();
-// request.open("GET", "https://api.corona-zahlen.org/districts");
-// request.send();
-// request.onload = () => {
-//     if (request.status === 200) {
-//         console.log(JSON.parse(request.response))}
-//     else { console.log(`error ${request.status}`)
-// }
-// };
+fetch('https://api.corona-zahlen.org/districts')
+.then(data => data.json())
+.then(data => {
+    console.log(data.data["09184"]);
+    let countyName = data.data["09184"].county;
+    let sevenDayIncidence = parseInt(data.data["09184"].weekIncidence);
+    
+    apiValues.push(sevenDayIncidence);
+    console.log(apiValues);
+    weeklyIncidence.innerText = sevenDayIncidence;
+    county.innerText = countyName;
+});
 
-
-// window.fetch('https://api.corona-zahlen.org/districts')
-// .then(response => {
-//     return response.json
-// }).then(json => console.log(json))
-
-
-// async
-// async function getData () {
-//     let response = await fetch('https://api.corona-zahlen.org/districts');
-//     let data = await response.json();
-//     return data;
-// }
-
-//getData().then(response => {
-//     //console.log(response.data);
-//     response.json());
-//     .then
-//     const parseData = JSON.parse(response);
-//     console.log(response);
-//     apiText.innerText = response;
-// })
 let prevValues = [129, 135, 132.4, 135.0, 124.1, 128.4, 123.5, 122.4, 122.4, 150.1, 174.3, 176.9, 180.6, 172.3].reverse();
-newValues = prevValues.concat([]);
+let newValues = prevValues.concat(apiValues);
+let newValues = [...prevValues, ...apiValues];
+
 const prevDates = ['19-04-2021', '20-04-2021', '21-04-2021', '22-04-2021', '23-04-2021', '25-04-2021', '26-04-2021', '27-04-2021', 
 '28-04-2021', '29-04-2021', '30-04-2021', '01-05-2021', '02-05-2021', '03-05-2021'];
 prevDates2 = prevDates.map((d) => {
@@ -44,18 +27,7 @@ prevDates2 = prevDates.map((d) => {
 console.log(prevDates2);
 //console.log(new Date(prevDates));
 
-fetch('https://api.corona-zahlen.org/districts')
-.then(data => data.json())
-.then(data => {
-    //console.log(data.data["09184"]);
-    let countyName = data.data["09184"].county;
-    let sevenDayIncidence = parseInt(data.data["09184"].weekIncidence);
-    weeklyIncidence.innerText = sevenDayIncidence;
-    county.innerText = countyName;
-});
-
-CHART = document.getElementById('lineChart');
-console.log(CHART);
+CHART = document.getElementById('lineChart');   
 let line_Chart = new Chart(CHART, {
     type: 'line',
     data:  {
@@ -87,7 +59,6 @@ let line_Chart = new Chart(CHART, {
         scales: {
             y: {
                 beginAtZero: true
-                
                 
             }
         }
